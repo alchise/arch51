799b829ef2a13c78df68be6dab6d259041acd29c
Alchise <alchise@Archlinux.localdomain>
2021-06-11 17:51:44 -0500

    Prueba de menu - alfa v1.01

--- arch51.sh
+++ arch51.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
 
-echo "*******************************"
-echo "Instalador de Archlinux arch51"
-echo "     Por: Alchise 2021"
-echo "*******************************"
+echo "-------------------------------------------------------"
+echo "      Instalador de Archlinux arch51 - UEFI"
+echo "               Por: Alchise 2021"
+echo "-------------------------------------------------------"
+echo "¡EL DISCO DEBE TENER TABLA GPT Y UBICADO EN /DEV/SDA!"
+echo "-------------------------------------------------------"
 
-opcion=0
+opcteclado=0
 
-while [ $opcion -ne 6 ]; do
+while [ $opcteclado -ne 4 ]; do
+
+	echo "Seleccione la distribucion de teclado:"	
 	echo "************************************************"
-	echo "1. Configurar teclado temporalmente 'es'"
-	echo "2. Verificar modo UEFI"
-	echo "3. Actualizar el reloj del sistema"
-	echo "4. Identificar discos"
-	echo "5. Crear tabla de particiones GPT en '/dev/sda'"
-	echo "6. Salir"
+	echo "1. Español - España - 'es'        - ()=?¿"
+	echo "2. Español - Latino - 'la-latin1' - ()=?¡"
+	echo "3. Inglés  - EEUU   - 'us'        - ()-+" 
+	echo "4. Salir"
 	echo "************************************************"
-	echo Selecciona una opcion:
-	read opcion
+	echo "Selecciona una opcion:"
 
-	case $opcion in
+	read opcteclado
+
+	case $opcteclado in
 		1) loadkeys es ;;
-		2) ls /sys/firmware/efi/efivars ;;
-		3) timedatectl set-ntp true ;;
-		4) lsblk ;;
-		5) gdisk /dev/sda ;;
-		6) echo "¡Adios!" ;;
-		*) echo Opcion no valida
-		   opcion=0 ;;	
+		2) loadkeys la-latin1 ;;
+		3) loadkeys us ;;   
+		4) echo "Ok"  ;;                                                                     
+		*) echo "Opcion no valida"
+		   opcteclado=0;;	
 	esac
-	echo Pulsa para continuar ....
+
+	echo "Pulsa para continuar ...."
+
 	read p
 done
+
